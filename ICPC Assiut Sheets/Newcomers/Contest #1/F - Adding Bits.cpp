e38f3fdeb2a3de491a13156e019094d39b796a9d
Osama Rafat <osama.rafat100@gmail.com>
2022-07-28 00:37:58 +0200

    Updates

---
+++ ICPC Assiut Sheets/Newcomers/Contest #1/F - Adding Bits.cpp
@@ -0,0 +1,55 @@
+/*********************************************************
+       "ولولا الأمل في بكرة إني ممكن اوصلك مكنتش اقدر اكمل"
+***********************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+void OSAMA() { ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0); }
+void File()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.in", "r", stdin);
+  freopen("output.in", "w", stdout);
+#endif
+}
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+#define sz(n) int(n.size())
+#define endl '\n'
+#define watch(x) cout << (#x) << " = " << x << endl
+ll gcd(ll x, ll y)
+{
+  return (!y) ? x : gcd(y, x % y);
+}
+ll lcm(ll x, ll y) { return ((x / gcd(x, y)) * y); }
+const int N = 2e5 + 5;
+//===========================================================
+
+void solve()
+{
+  int a, b;
+  cin >> a >> b;
+  int ans = 0;
+  for (int i = 0; i < 32; i++)
+  {
+    int x = a % 2, y = b % 2;
+    if (x + y == 1)
+      ans += pow(2, i);
+    a /= 2, b /= 2;
+  }
+  cout << ans;
+}
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+int main()
+{
+  //File();
+  OSAMA();
+  int t = 1;
+  // cin >> t;
+
+  while (t--)
+  {
+
+    solve();
+  }
+}
---
+++ ICPC Assiut Sheets/Newcomers/README.md
@@ -0,0 +1,4 @@
+# ICPC Assiut Materials
+https://docs.google.com/spreadsheets/u/0/d/1EbbsotAwb0zuuwxyzs8l2qh8twqw-sNcNbAjCK1kXaE/htmlview#
+# Codeforces for this sheet 
+https://codeforces.com/group/MWSDmqGsZm/contests
